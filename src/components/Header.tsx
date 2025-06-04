6d32c5e8fa4c6d3b1b5e58dc122a804215549d76
nahtanpng <nathan.ferreiira.dev@gmail.com>
2025-06-04 15:16:28 -0300

    refactor: improve code formatting and consistency across components

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,68 +1,70 @@
-import {DotsNineIcon} from "@phosphor-icons/react";
-import {useEffect, useState} from "react";
-import {Menu} from "./Menu";
+import { DotsNineIcon } from "@phosphor-icons/react";
+import { useEffect, useState } from "react";
+import { Menu } from "./Menu";
 import logo from "/assets/logo.png";
-import {useNavigate} from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 export const Header = () => {
-    const [isMenuOpen, setIsMenuOpen] = useState(false);
-    const navigate = useNavigate()
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const navigate = useNavigate();
 
-    const toggleMenu = () => {
-        setIsMenuOpen(!isMenuOpen);
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  const closeMenu = () => {
+    setIsMenuOpen(false);
+  };
+
+  // Close menu on ESC key or outside click
+  useEffect(() => {
+    const handleKeyDown = (event: KeyboardEvent) => {
+      if (event.key === "Escape") {
+        closeMenu();
+      }
     };
 
-    const closeMenu = () => {
-        setIsMenuOpen(false);
+    const handleClick = (event: MouseEvent) => {
+      if (
+        !(event.target instanceof Element) ||
+        (!event.target.closest(".menu") && !event.target.closest(".menu-icon"))
+      ) {
+        closeMenu();
+      }
     };
 
-    // Close menu on ESC key or outside click
-    useEffect(() => {
-        const handleKeyDown = (event: KeyboardEvent) => {
-            if (event.key === "Escape") {
-                closeMenu();
-            }
-        };
+    document.addEventListener("keydown", handleKeyDown);
+    document.addEventListener("click", handleClick);
 
-        const handleClick = (event: MouseEvent) => {
-            if (
-                !(event.target instanceof Element) ||
-                (!event.target.closest(".menu") && !event.target.closest(".menu-icon"))
-            ) {
-                closeMenu();
-            }
-        };
+    return () => {
+      document.removeEventListener("keydown", handleKeyDown);
+      document.removeEventListener("click", handleClick);
+    };
+  }, []);
 
-        document.addEventListener("keydown", handleKeyDown);
-        document.addEventListener("click", handleClick);
-
-        return () => {
-            document.removeEventListener("keydown", handleKeyDown);
-            document.removeEventListener("click", handleClick);
-        };
-    }, []);
-
-    return (
-        <>
-            <header
-                className="bg-transparent fixed top-0 w-full flex justify-between items-center px-6 md:px-24 py-8 z-40">
-                <a onClick={() => navigate("/")} className="navbar-brand cursor-pointer">
-                    <img
-                        src={logo}
-                        alt="Logo"
-                        width="137"
-                        height="39"
-                        className="inline-block align-top"
-                    />
-                </a>
-                <div
-                    className="menu-icon cursor-pointer text-white text-4xl animate-pulse hover:text-[#972620] transition-colors"
-                    onClick={toggleMenu}
-                >
-                    <DotsNineIcon size={32}/>
-                </div>
-            </header>
-            <Menu isOpen={isMenuOpen} onClose={closeMenu}/>
-        </>
-    );
+  return (
+    <>
+      <header className="bg-transparent fixed top-0 w-full flex justify-between items-center px-6 md:px-24 py-8 z-40">
+        <a
+          onClick={() => navigate("/")}
+          className="navbar-brand cursor-pointer"
+        >
+          <img
+            src={logo}
+            alt="Logo"
+            width="137"
+            height="39"
+            className="inline-block align-top"
+          />
+        </a>
+        <div
+          className="menu-icon cursor-pointer text-white text-4xl animate-pulse hover:text-[#972620] transition-colors"
+          onClick={toggleMenu}
+        >
+          <DotsNineIcon size={32} />
+        </div>
+      </header>
+      <Menu isOpen={isMenuOpen} onClose={closeMenu} />
+    </>
+  );
 };
--- src/pages/BeyondBasicsPage.tsx
+++ src/pages/BeyondBasicsPage.tsx
@@ -1,99 +1,110 @@
 export const BeyondBasicsPage = () => {
-    const cards = [
-        {
-            icon: "/assets/rastreamento.png",
-            title: "Rastreamento",
-            description: "Acompanhamos a localização de cada veículo com precisão através de GPS, facilitando a coordenação e garantindo uma resposta rápida a incidentes. Com essas informações valiosas, é possível direcionar motoristas para serviços de forma ágil e eficaz."
-        },
-        {
-            icon: "/assets/combustivel.png",
-            title: "Combustível",
-            description: "Acompanhamos a localização de cada veículo com precisão através de GPS, facilitando a coordenação e garantindo uma resposta rápida a incidentes. Com essas informações valiosas, é possível direcionar motoristas para serviços de forma ágil e eficaz."
-        },
-        {
-            icon: "/assets/manutencao.png",
-            title: "Manutenção",
-            description: "Indicamos através de checklists obtidos, manutenções preventivas de acordo com cada veículo indiviualmente, facilitando e evitando transtornos e manutenções corretivas."
-        }
-    ];
+  const cards = [
+    {
+      icon: "/assets/rastreamento.png",
+      title: "Rastreamento",
+      description:
+        "Acompanhamos a localização de cada veículo com precisão através de GPS, facilitando a coordenação e garantindo uma resposta rápida a incidentes. Com essas informações valiosas, é possível direcionar motoristas para serviços de forma ágil e eficaz.",
+    },
+    {
+      icon: "/assets/combustivel.png",
+      title: "Combustível",
+      description:
+        "Acompanhamos a localização de cada veículo com precisão através de GPS, facilitando a coordenação e garantindo uma resposta rápida a incidentes. Com essas informações valiosas, é possível direcionar motoristas para serviços de forma ágil e eficaz.",
+    },
+    {
+      icon: "/assets/manutencao.png",
+      title: "Manutenção",
+      description:
+        "Indicamos através de checklists obtidos, manutenções preventivas de acordo com cada veículo indiviualmente, facilitando e evitando transtornos e manutenções corretivas.",
+    },
+  ];
 
-    return (
-        <section
-            id="alem-do-basico"
-            className="relative min-h-screen w-full overflow-hidden"
+  return (
+    <section
+      id="alem-do-basico"
+      className="relative min-h-screen w-full overflow-hidden"
+    >
+      <div className="absolute inset-0 z-0">
+        <video
+          autoPlay
+          muted
+          loop
+          playsInline
+          className="w-full h-full object-cover"
         >
-            <div className="absolute inset-0 z-0">
-                <video
-                    autoPlay
-                    muted
-                    loop
-                    playsInline
-                    className="w-full h-full object-cover"
+          <source src="/assets/videos/bg_alem_do_basico.mp4" type="video/mp4" />
+        </video>
+        <div className="absolute inset-0 bg-black/50"></div>
+      </div>
+
+      <div className="relative z-10 container mx-auto px-6 pt-24 md:pt-32">
+        <div className="text-center mb-16">
+          <h1 className="text-4xl md:text-6xl lg:text-7xl text-white font-light">
+            /Além do <span className="text-[#972620]">Básico</span>
+          </h1>
+        </div>
+
+        <div className="mb-16">
+          <div className="w-full max-w-6xl mx-auto flex justify-start mb-16">
+            <div className="max-w-md">
+              <div className="relative pl-6">
+                <div className="absolute left-0 top-0 w-1 h-full bg-[#972620]"></div>
+                <p className="text-white text-base leading-relaxed">
+                  O Sistema SIG está em constante aprimoramento para incorporar
+                  as mais novas tecnologias do mercado. Assim, garantimos
+                  eficiência e inovação aliada a uma gestão moderna e eficaz.
+                </p>
+              </div>
+            </div>
+          </div>
+
+          <div className="w-full max-w-6xl mx-auto">
+            <div className="flex flex-wrap justify-center md:justify-between gap-6">
+              {cards.map((card, index) => (
+                <div
+                  key={index}
+                  className="bg-white/10 backdrop-blur-xs border border-white/20 rounded-4xl p-8 w-full max-w-xs flex flex-col hover:bg-white/20 transition-all duration-300"
                 >
-                    <source src="/assets/videos/bg_alem_do_basico.mp4" type="video/mp4"/>
-                </video>
-                <div className="absolute inset-0 bg-black/50"></div>
+                  <div className="flex flex-col">
+                    <img
+                      className="size-18 mb-4"
+                      src={card.icon}
+                      alt={card.title}
+                    />
+                    <h3 className="text-2xl font-medium text-white mb-4">
+                      {card.title}
+                    </h3>
+                  </div>
+
+                  <div>
+                    <p className="text-white/90 text-body leading-relaxed">
+                      {card.description}
+                    </p>
+                  </div>
+                </div>
+              ))}
             </div>
+          </div>
+        </div>
 
-            <div className="relative z-10 container mx-auto px-6 pt-24 md:pt-32">
-                <div className="text-center mb-16">
-                    <h1 className="text-4xl md:text-6xl lg:text-7xl text-white font-light">
-                        /Além do <span className="text-[#972620]">Básico</span>
-                    </h1>
-                </div>
-
-                <div className="mb-16">
-                    <div className="w-full max-w-6xl mx-auto flex justify-start mb-16">
-                        <div className="max-w-md">
-                            <div className="relative pl-6">
-                                <div className="absolute left-0 top-0 w-1 h-full bg-[#972620]"></div>
-                                <p className="text-white text-base leading-relaxed">
-                                    O Sistema SIG está em constante aprimoramento para incorporar as mais
-                                    novas tecnologias do mercado. Assim, garantimos eficiência e inovação
-                                    aliada a uma gestão moderna e eficaz.
-                                </p>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div className="w-full max-w-6xl mx-auto">
-                        <div className="flex flex-wrap justify-between gap-6">
-                            {cards.map((card, index) => (
-                                <div
-                                    key={index}
-                                    className="bg-white/10 backdrop-blur-xs border border-white/20 rounded-4xl p-8 w-full max-w-xs flex flex-col hover:bg-white/20 transition-all duration-300"
-                                >
-                                    <div className="flex flex-col">
-                                        <img className="size-18 mb-4" src={card.icon} alt={card.title}/>
-                                        <h3 className="text-2xl font-medium text-white mb-4">
-                                            {card.title}
-                                        </h3>
-                                    </div>
-
-                                    <div>
-                                        <p className="text-white/90 text-body leading-relaxed">
-                                            {card.description}
-                                        </p>
-                                    </div>
-                                </div>
-                            ))}
-                        </div>
-                    </div>
-                </div>
-
-                <div className="w-full max-w-6xl mx-auto">
-                    <div className="flex items-center justify-end">
-                        <div className="flex items-center space-x-3 cursor-pointer group">
-                            <p className="text-white text-sm transition-colors">
-                                Conheça mais um <br/> pouco das nossas
-                                <br/>
-                                <span className="font-bold">soluções</span>
-                            </p>
-                            <img className="size-10" src="/assets/link-icon.png" alt="Link icon"/>
-                        </div>
-                    </div>
-                </div>
+        <div className="w-full max-w-6xl mx-auto">
+          <div className="flex items-center justify-end">
+            <div className="flex items-center space-x-3 cursor-pointer group">
+              <p className="text-white text-sm transition-colors">
+                Conheça mais um <br /> pouco das nossas
+                <br />
+                <span className="font-bold">soluções</span>
+              </p>
+              <img
+                className="size-10"
+                src="/assets/link-icon.png"
+                alt="Link icon"
+              />
             </div>
-        </section>
-    );
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
